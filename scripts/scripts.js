1b1781d05db037ac37c09297265cf22a35473aa3
Jean Ambait <jeanclaudetteambait@gmail.com>
2011-12-24 19:15:48 +0800

    Grouped html text of profile.txt

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,8 +12,11 @@ $(document).ready(function(){
 	
 	$('a#profile').click(function(){
 		var path = 'txts/profile.txt';
-		var element = $('.holder').has('div#text');
-		$('<div id="text"></div>').appendTo('.holder');
+		var page = "div#text1";
+		var element = $('.holder').has(page);
+		page = page.substring(4);
+		var holderHeight = $('.holder').height() - 150;
+		$('<div id="' + page + '"></div>').appendTo('.holder');
 		$.get(path, function(result){
 			if(element.length < 1){
 				if(result){
@@ -21,16 +24,31 @@ $(document).ready(function(){
 					$('#photo').slideUp('slow');
 					$.each(lines, function(key, value){
 						if(value.length > 1){
-							var index = value.lastIndexOf('*');
-							if(index > -1){
-								$('<h2>' + value.substring(index + 1) + '</h2>').appendTo('#text');
-							}else{
-								index = value.indexOf('+');
+							var index = 0;
+							var textHeight = $('#' + page).height();
+							if(textHeight < holderHeight){
+								index = value.lastIndexOf('*');
 								if(index > -1){
-									$('<i>' + value + '</i>').appendTo('#text');
+									$('<h2>' + value.substring(index + 1) + '</h2>').appendTo('#text');
 								}else{
-									$('<p>' + value + '</p>').appendTo('#text');
+									index = value.indexOf('>');
+									if(index > -1){
+										$('<i>' + value + '</i>').appendTo('#' + page);
+									}else{
+										$('<p>' + value + '</p>').appendTo('#' + page);
+									}
 								}
+							}else{
+								page = page.substring(0, page.length - 1) + (parseInt(page.charAt(page.length - 1)) + 1);
+								$('<div id="' + page + '"></div>').appendTo('.holder');	
+								index = value.indexOf('>');
+								
+								if(index > -1){
+									$('<i>' + value + '</i>').appendTo('#' + page);
+								}else{
+									page = page.substring(0, page.length - 1) + (parseInt(page.charAt(page.length - 1)) - 1);
+									$('<p>' + value + '</p>').appendTo('#' + page);
+								}							
 							}
 						}
 					});
