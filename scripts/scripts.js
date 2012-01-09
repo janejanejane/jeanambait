a3789631bf72014c26c9295f58bc079eeaa5ff06
Jean Ambait <jeanclaudetteambait@gmail.com>
2012-01-10 00:38:29 +0800

    Checks browser input then displays info.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,16 @@
 $(document).ready(function(){
 	var location = document.location;	
+	var history = window.history;	
+	var page = "page";
+	
+	page = getUrlPageValue(page);
+	page = (page == null) ? null : (page+'.html');
+	displayInfo(page);
 	
 	$('a').click(function(evt){
 		evt.preventDefault();
 		var link = $(this).attr('href');
-		
-		var history = window.history;
-		
-		if(link != "index.html"){
-			var title = link.substring(0, link.indexOf('.'));
-			var photo = $('#photo').slideUp('slow');
-			
-			history.replaceState('',title,title);
-			link = "pages/" + link;
-			$.when(photo).done(function(){
-				$.get(link, function(data){
-						var divDetails = $('#details div');
-						if(divDetails.length > 0){
-							$('#details div').remove();
-						}
-						$('#details').addClass('details');
-						$('#details').append(data);
-					});
-					$('#puller').show();
-				});
-		}else{
-			setUrlToDomainName(location, link);
-			if(!$('#photo').is(":visible")){
-				pullPicture();
-			}
-		}
+		displayInfo(link);
 	});
 	
 	
@@ -43,6 +24,17 @@ $(document).ready(function(){
 			$('#details').removeClass('details');
 			$('#puller').hide();
 			$('#photo').slideDown('slow');
+	}
+	
+	function getUrlPageValue(page) {
+		return decodeURIComponent(
+			(location.search.match(RegExp('[?|&]' + page + '=(.+?)(&|$)'))||[,null])[1] //returns the value of ?page= after match (e.g. ?page=me,me)
+			// (.)any single character (+)matched 1 or more times (?)zero or more times (&)ends string  ($)matched the end of the string
+		); 
+		
+		/*return decodeURI(
+			(RegExp('[?|&]' + page + '=(.+?)(&|$)').exec(location.search))
+		);*/
 	}
 	
 	function getDomainName(location, link){
@@ -65,4 +57,45 @@ $(document).ready(function(){
 		location = getDomainName(location, link);
 		history.replaceState('','home',location);	
 	}
+	
+	function displayInfo(link){
+		if(link != "null"){
+			if(link != "index.html"){
+				var title = link.substring(0, link.indexOf('.'));
+				link = "pages/" + link;
+				
+				if(fileExists(getDomainName(location, null) + link)){
+					var photo = $('#photo').slideUp('slow');
+					
+					history.replaceState('',title,'?page=' + title);
+					
+					$.when(photo).done(function(){
+						$.get(link, function(data){
+								var divDetails = $('#details div');
+								if(divDetails.length > 0){
+									$('#details div').remove();
+								}
+								$('#details').addClass('details');
+								$('#details').append(data);
+								$('#details').slideDown('slow');
+							});
+							$('#puller').show();
+						});
+				}
+			}else{
+				setUrlToDomainName(location, link);
+				if(!$('#photo').is(":visible")){
+					pullPicture();
+				}
+			}
+		}
+	}
+	
+	function fileExists(url){
+		var http = new XMLHttpRequest();
+		http.open('HEAD', url, false);
+		http.send();
+		return http.status!=404;
+	}
+	
 });
